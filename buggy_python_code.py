44962219c9e861440b9e3c390de61e8e4f8a4b3a
MindstormsBlock <jakub.fischer1@gmail.com>
2026-05-06 19:09:34 +0200

    more fixes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -18,15 +18,18 @@ class Person:
         self.name = name
 
 
-def print_nametag(format_string, person):
-    print(format_string.format(person=person))
+def print_nametag(person):
+    print(person.name)
 
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    # exec(f"import urllib{urllib_version} as urllib", globals())
-    # Fetch and print the requested URL
+    if (urlib_version == 2):
+        import urllib2 as urllib
+    elif (urllib_version == 3):
+        pass
 
+    # Fetch and print the requested URL
     try:
         http = urllib.PoolManager()
         request = http.request('GET', url)
